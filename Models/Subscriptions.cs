1eaf4c9c917ecfa9bd1e6726bf3e6297a67d6efd
Sean Dorris <seanc.dorris@gmail.com>
2023-08-07 18:37:09 -0500

    all classes created

---
+++ Models/Subscriptions.cs
@@ -0,0 +1,10 @@
+﻿namespace rare.Models
+{
+    public class Subscriptions
+    {
+        public int Id { get; set; }
+        public int FollowerId { get; set; }
+        public int AuthorId { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ Models/Tags.cs
@@ -0,0 +1,8 @@
+﻿namespace rare.Models
+{
+    public class Tags
+    {
+        public int Id { get; set; }
+        public string label { get; set; }
+    }
+}
